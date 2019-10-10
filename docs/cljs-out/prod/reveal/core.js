625d0fa857a77d7d67230b9b38cdfb849c21cd45
Wesley Matson <wmatson@clearwateranalytics.com>
2019-10-10 15:49:52 -0600

    Seeing if the . in the subpath was messing up stuff

--- docs/cljs-out/prod/reveal/core.js
+++ docs/cljs-out/prod/reveal/core.js
@@ -6,15 +6,15 @@ goog.require('clojure.string');
 goog.require('goog.dom');
 goog.require('hiccups.runtime');
 goog.require('reveal.slides');
-reveal.core.options = cljs.core.clj__GT_js(new cljs.core.PersistentArrayMap(null, 5, [cljs.core.cst$kw$controls,true,cljs.core.cst$kw$progress,true,cljs.core.cst$kw$transition,"slide",cljs.core.cst$kw$slideNumber,false,cljs.core.cst$kw$dependencies,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.PersistentArrayMap(null, 2, [cljs.core.cst$kw$src,"node_modules/reveal.js/plugin/notes/notes.js",cljs.core.cst$kw$async,true], null),new cljs.core.PersistentArrayMap(null, 2, [cljs.core.cst$kw$src,"node_modules/reveal.js/plugin/highlight/highlight.js",cljs.core.cst$kw$async,true], null)], null)], null));
+reveal.core.options = cljs.core.clj__GT_js(new cljs.core.PersistentArrayMap(null, 5, [cljs.core.cst$kw$controls,true,cljs.core.cst$kw$progress,true,cljs.core.cst$kw$transition,"slide",cljs.core.cst$kw$slideNumber,false,cljs.core.cst$kw$dependencies,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.PersistentArrayMap(null, 2, [cljs.core.cst$kw$src,"node_modules/reveal/plugin/notes/notes.js",cljs.core.cst$kw$async,true], null),new cljs.core.PersistentArrayMap(null, 2, [cljs.core.cst$kw$src,"node_modules/reveal/plugin/highlight/highlight.js",cljs.core.cst$kw$async,true], null)], null)], null));
 /**
  * Get list of all slides and convert them to html strings.
  */
 reveal.core.convert = (function reveal$core$convert(){
 var slides = reveal.slides.all();
 return clojure.string.join.cljs$core$IFn$_invoke$arity$1(cljs.core.map.cljs$core$IFn$_invoke$arity$2(((function (slides){
-return (function (p1__14783_SHARP_){
-return cljs.core.str.cljs$core$IFn$_invoke$arity$1(hiccups.runtime.render_html(p1__14783_SHARP_));
+return (function (p1__11411_SHARP_){
+return cljs.core.str.cljs$core$IFn$_invoke$arity$1(hiccups.runtime.render_html(p1__11411_SHARP_));
 });})(slides))
 ,slides));
 });
